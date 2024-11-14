f38dfd3089efa6b647e619dbde1496b0a554124c
oliver <oshmusic11@gmail.com>
2024-11-14 17:42:04 -0500

    First structure for Projects data (and potential other site data) handling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,9 @@
+import projectsList from "../data/projects-list.json";
+
+const projects = projectsList.projects;
 
 const Projects = () => {
+    console.log(projects);
     return (
         <p>TODO</p>
     );
