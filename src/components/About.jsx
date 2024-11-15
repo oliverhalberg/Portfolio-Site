bf82ae7f7060f7f65ee46c1010c8d6c5f3148837
oliver <oshmusic11@gmail.com>
2024-11-15 17:38:24 -0500

    Added first draft of text to About page and edited project descriptions in src/data/projects-list.json

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,26 @@ const About = () => {
     return (
         <>
             <h2>About Me</h2>
-            <p>Todo</p>
+            <p>I am an amateur software developer, open-source enthusiast, and ANOTHER ADJECTIVE who enjoys learning new technologies and skills. Outside of computer science, I am a musician and proud nerd who can often be found playing video games or tabletop role-playing games, reading, or playing music.</p>
             <h2>Education</h2>
-            <p>Also todo</p>
+            <p>Vassar College</p> 
+            <p>August 2020 - May 2024</p>
+            <p>Bachelor of Arts in Computer Science</p>
+            <p>Correlate in French and Francophone Studies</p>
+            <p>Graduated with Departmental and General Honors</p>
+            <h3>Relevant Coursework</h3>
+            <p>
+                Computer Science I: Problem Solving and Abstraction, Computer Science II: Data Structures and Algorithms, 
+                Foundations of Computer Science, Computer Organization,Theory of Computation, 
+                Computer Science III: Software Design and Implementation, Analysis of Algorithms, Operating Systems, 
+                Bioinformatics, Compilers, Computational Linguistics
+            </p>
+            <h2>Other courses/Certifications</h2>
+            <h3>Humble Bundle: Full Stack JavaScript + Learn React Track</h3>
+            <p>
+                In summer 2024, I enrolled in a series of courses through Treehouse in order to learn the basics of full-stack 
+                web development, including JavaScript, Node.js, Express.js, SQL basics, and React.js.
+            </p>
             {/* Treehouse widget here? */}
         </>
     );
