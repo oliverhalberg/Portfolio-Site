f35554b1ef46384b9f6c836e5581b67088872a00
oliver <oshmusic11@gmail.com>
2024-11-15 17:50:43 -0500

    Added code for displaying images in Project components

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,11 +3,14 @@ const Project = ({ data }) => {
     return (
         <div>
             <h2>{data.title}</h2>
+            {/* Include image, if present (not all projects might have associated images) */}
+            {data.imgPath ? <img src={data.imgPath} /> : null}
             <h3>Technologies used:</h3>
             <ul>
                 {data.technologies.map((tech) => <li>{tech}</li>)}
             </ul>
             <p>{data.description}</p>
+            {/* Include link to source code on Github, if present */}
             {data.githubLink ? <a href={data.githubLink}>Github</a> : null}
         </div>
     );
