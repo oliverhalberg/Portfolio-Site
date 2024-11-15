bfbb05764581e638dd53b850773d416f71b99389
oliver <oshmusic11@gmail.com>
2024-11-15 17:45:36 -0500

    Added first draft of text to Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => {
         <>
             <h3>Portfolio Website - Oliver Halberg</h3>
             <p>Thanks for visiting! I am currently in the process of building out this website using React. If you're interested, feel free to check out the source code <a href="https://github.com/oliverhalberg/Portfolio-Site">here</a>.</p>
+            <p>
+                I am a recent computer science graduate seeking remote software development roles. I have experience with a variety
+                of technologies, including Android development, web development, command-line tools, and scripting,
+                and have worked with a variety of programming languages such as Java, Python, JavaScript, and OCaml.
+            </p>
         </>
     );
 }
