544c3a0627f651dc37accda921f4fc94e762cadd
oliver <oshmusic11@gmail.com>
2024-11-19 19:51:01 -0500

    started styling Project components; images are now required for the component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,34 +1,31 @@
 import PropTypes from 'prop-types';
 const Project = ({ data }) => {
     return (
-        <div className='project'>
+        <div className='project'> {/* TODO: Add reverse */}
             <div className='projectText'>
                 <h2>{data.title}</h2>
                 <h3>Technologies used:</h3>
-                <ul>
-                    {data.technologies.map((tech) => <span className='projectTech'>{tech}</span>)}
-                </ul>
+                {data.technologies.map((tech) => <span className='projectTech' key={tech}>{tech}</span>)}
                 <p>{data.description}</p>
+                {/* Include link to source code on Github, if present */}
+                {data.githubLink ? <p><a href={data.githubLink} className='projectGithubLink'>Github</a></p> : null}
             </div>
-            {/* Include image, if present (not all projects might have associated images) */}
-            <div className={data.imgPath ? 'projectImgDiv' : 'projectImgDivNoImg'}>
-                {data.imgPath ? <img src={data.imgPath} className='projectImg' /> : null}
+            <div className='projectImgDiv'>
+                <img src={data.imgPath} className='projectImg' />
             </div>
-            {/* Include link to source code on Github, if present */}
-            {data.githubLink ? <a href={data.githubLink} className='projectGithubLink'>Github</a> : null}
         </div>
     );
 }
 
 export default Project;
 
-Project.PropTypes = {
+Project.propTypes = {
     data: PropTypes.shape({
         id: PropTypes.number.isRequired,
         technologies: PropTypes.arrayOf(PropTypes.string).isRequired,
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
         githubLink: PropTypes.string,
-        imgPath: PropTypes.string
+        imgPath: PropTypes.string.isRequired
     })
 }
