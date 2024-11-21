c1e921416431a74107b80a5c29807d826862b24f
oliver <oshmusic11@gmail.com>
2024-11-21 17:40:44 -0500

    Tweaks to About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,43 +8,54 @@ const About = () => {
                 <p>I am an amateur software developer, open-source enthusiast, and ANOTHER ADJECTIVE who enjoys learning new technologies and skills. Outside of computer science, I am a musician and proud nerd who can often be found playing video games or tabletop role-playing games, reading, or playing music.</p>
                 <p>Location: Shelburne, Vermont, United States</p>
             </div>
-            <div id="education">
-                <h2>Education</h2>
-                <p>Vassar College</p>
+            <h2>Education</h2>
+            <div id="educationContainer">
+                <h3>Vassar College</h3>
                 <p>August 2020 - May 2024</p>
-                <p>Bachelor of Arts in Computer Science</p>
-                <p>Correlate in French and Francophone Studies</p>
-                <p>Graduated with Departmental and General Honors</p>
-                <h3>Relevant Coursework</h3>
-                <p>
-                    Computer Science I: Problem Solving and Abstraction, Computer Science II: Data Structures and Algorithms,
-                    Foundations of Computer Science, Computer Organization, Theory of Computation,
-                    Computer Science III: Software Design and Implementation, Analysis of Algorithms, Operating Systems,
-                    Bioinformatics, Compilers, Computational Linguistics
-                </p>
+                <div className="education">
+                    <div class="educationList">
+                        <p>
+                            Bachelor of Arts in Computer Science <br />
+                            Correlate in French and Francophone Studies <br />
+                            Graduated with Departmental and General Honors
+                        </p>
+                    </div>
+                    <div class="relevantCoursework">
+                        <p>
+                            <b>Relevant Coursework: </b>Computer Science I: Problem Solving and Abstraction, Computer Science II: Data Structures and Algorithms,
+                            Foundations of Computer Science, Computer Organization, Theory of Computation,
+                            Computer Science III: Software Design and Implementation, Analysis of Algorithms, Operating Systems,
+                            Bioinformatics, Compilers, Computational Linguistics
+                        </p>
+                    </div>
+                </div>
             </div>
             <div id="otherCourses">
                 <h2>Other courses/Certifications</h2>
-                <h3>Humble Bundle: Full Stack JavaScript + Learn React Track</h3>
-                <p>July 2024 - November 2024</p>
-                <p>
-                    In July 2024, I enrolled in a series of courses through Treehouse in order to learn the basics of full-stack
-                    web development, including JavaScript, Node.js, Express.js, SQL basics, and React.js.
-                </p>
+                <div className="course-cert">
+                    <h3>Humble Bundle: Full Stack JavaScript + Learn React Track</h3>
+                    <p>July 2024 - November 2024</p>
+                    <p>
+                        In July 2024, I enrolled in a series of courses through Treehouse in order to learn the basics of full-stack
+                        web development, including JavaScript, Node.js, Express.js, SQL basics, and React.js.
+                    </p>
+                </div>
                 <TreehouseWidget />
             </div>
             <div id="workExperience">
                 <h2>Relevant Work Experience</h2>
-                <h3>Computer Science Department Coach, Vassar College</h3>
-                <p>September 2022 - May 2024</p>
-                <p>
-                    During my third and fourth years at Vassar College, I was hired by the Computer Science Department to work as a student coach
-                    for three different intermediate-level computer science courses (Computer Science II: Data Structures and Algorithms,
-                    Computer Science III: Software Design and Implementation, and Computer Organization) over four semesters. As a coach, I assisted
-                    professors during labs and held office hours where students could come for help on course materials and assignments. I gained
-                    valuable experience debugging code, providing constructive feedback, and communicating computer science concepts in an understandable
-                    fashion.
-                </p>
+                <div className="work">
+                    <h3>Computer Science Department Coach, Vassar College</h3>
+                    <p>September 2022 - May 2024</p>
+                    <p>
+                        During my third and fourth years at Vassar College, I was hired by the Computer Science Department to work as a student coach
+                        for three different intermediate-level computer science courses (Computer Science II: Data Structures and Algorithms,
+                        Computer Science III: Software Design and Implementation, and Computer Organization) over four semesters. As a coach, I assisted
+                        professors during labs and held office hours where students could come for help on course materials and assignments. I gained
+                        valuable experience debugging code, providing constructive feedback, and communicating computer science concepts in an understandable
+                        fashion.
+                    </p>
+                </div>
             </div>
         </div>
     );
