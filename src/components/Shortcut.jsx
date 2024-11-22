29bfbbf568cec529b0333c393eac4dfbc287b06e
oliver <oshmusic11@gmail.com>
2024-11-22 17:40:20 -0500

    Changed <a> tags in Shortcut to NavLink tags; made Shortcut image change color on hover; styled Shortcut components on Home page

--- src/components/Shortcut.jsx
+++ src/components/Shortcut.jsx
@@ -1,18 +1,25 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 /* Library object matching paths to icons */
 const iconLibrary = {
     "About": "/src/data/icons/id-card-black.svg",
+    "About-white": "/src/data/icons/id-card-white.svg",
     "Projects": "/src/data/icons/computer-screen-black.svg",
-    "Resume": "/src/data/icons/resume-black.svg"
+    "Projects-white": "src/data/icons/computer-screen-white.svg",
+    "Resume": "/src/data/icons/resume-black.svg",
+    "Resume-white": "/src/data/icons/resume-white.svg"
 }
 
 const Shortcut = ({ path }) => {
+    const [hover, setHover] = useState(false);
+
     return (
         <div className='shortcutContainer'>
-            <a href={"/" + path.toLowerCase()}>
-                <img src={iconLibrary[path]} alt={path} />
-            </a>
+            <NavLink to={"/" + path.toLowerCase()} onMouseOver={()=> setHover(true)} onMouseOut={()=> setHover(false)}>
+                <img src={hover ? iconLibrary[path + "-white"] : iconLibrary[path]} alt={path} />
+            </NavLink>
             <p className='shortcutText'>{path}</p>
         </div>
     )
