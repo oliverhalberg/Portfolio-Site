e4a2944e2cf17c92b283ba324e5345404cf8a432
oliver <oshmusic11@gmail.com>
2024-11-27 15:30:15 -0500

    Styled Projects page for smaller screens

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const projects = projectsList.projects;
 const Projects = () => {
     return (
         <>
-        <h2>Projects</h2>
+        <h2 id="projectsTitle">Projects</h2>
         <div id="projectsContainer">
             {projects.map((project, i) => <div key={project.id}><Project key={project.id} data={project} reverse={i%2 === 0}/> {(i < projects.length - 1) ? <hr className="projectDivider"/> : null}</div>)}
         </div>
