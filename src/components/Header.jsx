3687053af4e97f2d6b3e61193f45ae9e12654aa3
oliver <oshmusic11@gmail.com>
2024-11-27 16:53:25 -0500

    First draft of responsive menu; nav still needs adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header>
             <span id="nameSpan">Oliver Halberg</span>
-            <ul id="navbar">
-                <li className="navbarLink"><NavLink to='/'>Home</NavLink></li>
-                <li className="navbarLink"><NavLink to='/about'>About</NavLink></li>
-                <li className="navbarLink"><NavLink to='/projects'>Projects</NavLink></li>
-                <li className="navbarLink"><NavLink to='/resume'>Resume</NavLink></li>
-            </ul>
+            <button
+                className="menuBtn"
+                onClick={() => setIsOpen(!isOpen)}
+                aria-label='Navigation Menu'
+                aria-expanded={isOpen}
+            >
+                <img src={isOpen ? "/src/data/icons/close_menu.svg" : "/src/data/icons/open_menu.svg"} />
+            </button>
+            <nav className={`nav ${isOpen ? 'open' : ''}`}>
+                <ul id="navbar">
+                    <li className="navbarLink"><NavLink to='/'>Home</NavLink></li>
+                    <li className="navbarLink"><NavLink to='/about'>About</NavLink></li>
+                    <li className="navbarLink"><NavLink to='/projects'>Projects</NavLink></li>
+                    <li className="navbarLink"><NavLink to='/resume'>Resume</NavLink></li>
+                </ul>
+            </nav>
         </header>
     );
 }
