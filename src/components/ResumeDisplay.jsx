89581c5988ce17fa532f98352e13ecebd99e8220
oliver <oshmusic11@gmail.com>
2024-11-26 19:45:07 -0500

    Added notice for small screens to Resume page; moved media query to bottom of src/index.css to solve a problem where styles would not override

--- src/components/ResumeDisplay.jsx
+++ src/components/ResumeDisplay.jsx
@@ -3,6 +3,7 @@ const pdfpath = "/src/data/oliverhalberg.pdf#zoom=100%";
 const ResumeDisplay = () => {
     return (
         <div>
+            <p id="smallScreenNotice">The pdf viewer tends to work better on larger screens. Sorry for the inconvenience!</p>
             <object id="resumeDisplay" data={pdfpath} type="application/pdf">
                 <p>Something went wrong, sorry!</p>
             </object>
