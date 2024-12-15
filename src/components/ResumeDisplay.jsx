923867deea0657e8055beea491ebcdf212398488
oliver <oshmusic11@gmail.com>
2024-12-14 23:58:27 -0500

    Added notes for potential fixes (to be tried later) to src/components/ResumeDisplay.jsx

--- src/components/ResumeDisplay.jsx
+++ src/components/ResumeDisplay.jsx
@@ -1,5 +1,12 @@
 //for whatever reason, the automatic zoom looks bad on larger screen sizes - it adds a small white bar to the right of the pdf
 const pdfpath = "/pdfs/oliverhalberg.pdf#zoom=100%";
+//when running in dev, this works without issue
+//on the built site, this component displays the website inside of it instead,
+// displaying the NotFound component.
+// potential fixes:
+// - replace the <object> tags with Document from 'react-pdf'
+// - I think that the <object> tag's data property is being read by React Router as a path starting from root. restructure files?
+
 const ResumeDisplay = () => {
     return (
         <div>
