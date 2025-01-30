e5d562619e12ab22c77b8a53bc99cbe52fa089ec
oliver <oshmusic11@gmail.com>
2025-01-30 16:54:26 -0500

    Added clarifying subtitle to Projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,7 @@ const Projects = () => {
     return (
         <>
         <h2 id="projectsTitle">Projects</h2>
+        <p id="projectsSubtitle">Projects are presented in approximate chronological order, with the oldest ones listed first.</p>
         <div id="projectsContainer">
             {projects.map((project, i) => <div key={project.id}><Project key={project.id} data={project} reverse={i%2 === 0}/> {(i < projects.length - 1) ? <hr className="projectDivider"/> : null}</div>)}
         </div>
