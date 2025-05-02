e75c335b29be045f2c7ef21d3eb2133a4b697cff
oliver <oshmusic11@gmail.com>
2025-05-02 17:33:51 -0400

    Updated resume pdf and ResumeDisplay component

--- src/components/ResumeDisplay.jsx
+++ src/components/ResumeDisplay.jsx
@@ -1,5 +1,6 @@
-//for whatever reason, the automatic zoom looks bad on larger screen sizes - it adds a small white bar to the right of the pdf
-const pdfpath = "/pdfs/oliverhalberg.pdf#zoom=100%";
+//for whatever reason, in older versions of my resume, the automatic zoom looks bad on larger screen sizes - it adds a small white bar to the right of the pdf
+//note to self in case it happens again: add "#zoom=100%" to the end of the path
+const pdfpath = "/pdfs/oliverhalberg.pdf";
 
 const ResumeDisplay = () => {
     return (
