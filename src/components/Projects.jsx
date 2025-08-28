0a6d0c956f09fe1c165e5e2b3cead62c4a20ef43
oliver <oshmusic11@gmail.com>
2025-08-28 17:16:51 -0400

    Added note to Projects page subtitle stating that this page may be out of date along with a link to my Github page; additional styling for the Projects page subtitle

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
     return (
         <>
         <h2 id="projectsTitle">Projects</h2>
-        <p id="projectsSubtitle">Projects are presented in approximate chronological order, with the oldest ones listed first.</p>
+        <p id="projectsSubtitle">Projects are presented in approximate chronological order, with the oldest ones listed first. This page may not be entirely up to date; check out my <a href="https://github.com/oliverhalberg">Github page</a> to see what projects are currently in progress.</p>
         <div id="projectsContainer">
             {projects.map((project, i) => <div key={project.id}><Project key={project.id} data={project} reverse={i%2 === 0}/> {(i < projects.length - 1) ? <hr className="projectDivider"/> : null}</div>)}
         </div>
